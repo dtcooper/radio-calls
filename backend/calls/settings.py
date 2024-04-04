19da9d767954d05f84056876f5648a451f4a07b8
David Cooper <david@dtcooper.com>
2024-04-04 13:46:38 +0000

    Fix nginx prod container paths, remove password validation requirements

--- backend/calls/settings.py
+++ backend/calls/settings.py
@@ -94,21 +94,6 @@ DATABASES = {
     }
 }
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
-
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
